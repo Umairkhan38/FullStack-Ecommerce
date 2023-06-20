3eba76bc26d926236141ed380181ef8ef35000cf
Umair Khan <uk125896@gmail.com>
2023-06-20 23:45:05 +0530

    Commiting with brands & Categories API's

--- src/features/product-list/ProductListAPI.js
+++ src/features/product-list/ProductListAPI.js
@@ -1,22 +1,67 @@
-// A mock function to mimic making an async request for data
-export function fetchAllProducts(amount = 1) {
-  return new Promise(async(resolve) =>{
-    const response = await fetch('http://localhost:8080/products');
+export function fetchAllProducts() {
+  return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products') 
     const data = await response.json()
     resolve({data})
- });
+  }
+  );
 }
 
-export function fetchProductsByFilters(filter) {
-
-    let queryString='';
+export function fetchProductsByFilters(filter,sort,pagination) {
+  // filter = {"category":["smartphone","laptops"]}
+  // sort = {_sort:"price",_order="desc"}
+  // pagination = {_page:1,_limit=10} 
+  // TODO : on server we will support multi values in filter
+  let queryString = '';
   for(let key in filter){
-    queryString+=`${key}=${filter[key]}&`
+    const categoryValues = filter[key];
+    console.log("cat val", categoryValues);
+    
+    if(categoryValues.length){
+      const lastCategoryValue = categoryValues[categoryValues.length-1]
+      queryString += `${key}=${lastCategoryValue}&`
+    }
   }
 
-  return new Promise(async(resolve) =>{
-    const response = await fetch('http://localhost:8080/products?'+queryString);
+  for(let key in sort){
+    queryString += `${key}=${sort[key]}&`
+  }
+
+  
+  console.log(pagination)
+  for(let key in pagination){
+    queryString += `${key}=${pagination[key]}&`
+  }
+
+
+  return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products?'+queryString) 
+    const data = await response.json()
+    const totalItems = await response.headers.get('X-Total-Count')
+    resolve({data:{products:data,totalItems:+totalItems}})
+  }
+  );
+}
+
+
+export function fetchCategories() {
+  return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/categories') 
     const data = await response.json()
     resolve({data})
- });
+  }
+  );
+}
+
+export function fetchBrands() {
+  return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/brands') 
+    const data = await response.json()
+    resolve({data})
+  }
+  );
 }
