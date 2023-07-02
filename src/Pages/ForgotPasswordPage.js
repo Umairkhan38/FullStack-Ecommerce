13619356bc1ebe89b23c543c9d23f9fdf483f930
Umair Khan <uk125896@gmail.com>
2023-07-02 23:14:42 +0530

    signout & forgot password modified

--- src/Pages/ForgotPasswordPage.js
+++ src/Pages/ForgotPasswordPage.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import ForgotPassword from '../features/auth/Components/ForgotPassword'
-import NavBar from '../features/Navbar/Navbar'
 
 function ForgotPasswordPage() {
   return (
     <div>
-        <NavBar>
             <ForgotPassword />
-        </NavBar> 
     </div>
   )
 }
